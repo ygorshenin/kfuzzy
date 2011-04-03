f292755eb5f74ae55b32c8ce76ae6598addeacf0
ygorshenin <ygorshenin@google.com>
2011-04-03 23:39:34 +0400

    Added references to google flags and google tests libraries.

--- kfuzzy.cc
+++ kfuzzy.cc
@@ -1,3 +1,20 @@
+#ifdef HOME
+
+#include <gflags/gflags.h>
+#include <gtest/gtest.h>
+
+DEFINE_bool(run_all_tests, false, "run all tests");
+
+#endif
+
+
 int main(int argc, char **argv) {
+#ifdef HOME
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  if (FLAGS_run_all_tests) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+  }
+#endif
   return 0;
 }
--- makefile
+++ makefile
@@ -1,8 +1,8 @@
 kfuzzy: kfuzzy.o
-	g++ -o kfuzzy kfuzzy.o
+	g++ -o kfuzzy kfuzzy.o -L/usr/local/lib -lgflags -lgtest
 
 kfuzzy.o: kfuzzy.cc
-	g++ -c -o kfuzzy.o kfuzzy.cc
+	g++ -DHOME -g -o kfuzzy.o -c kfuzzy.cc
 
 clear:
 	rm kfuzzy.o
