4f79fc1a96acac338a76a0e886459bb01fcd1b79
ygorshenin <ygorshenin@google.com>
2011-04-18 02:31:12 +0400

    Added Pair class.

---
+++ java/src/kfuzzy/utils/Pair.java
@@ -0,0 +1,12 @@
+package kfuzzy.utils;
+
+
+public class Pair<K, V> {
+    public final K first;
+    public final V second;
+
+    public Pair(K first, V second) {
+	this.first = first;
+	this.second = second;
+    }
+}
