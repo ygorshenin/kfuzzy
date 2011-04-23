7ba28efd3cef2741d98fccce6b84c688eeeec2d4
ygorshenin <ygorshenin@google.com>
2011-04-23 14:56:10 +0400

    Added javadocs to MostDistantInterface.

--- java/src/kfuzzy/algo/MostDistantInterface.java
+++ java/src/kfuzzy/algo/MostDistantInterface.java
@@ -1,10 +1,23 @@
 package kfuzzy.algo;
 
-import kfuzzy.math.*;
+import kfuzzy.math.Vector;
 
 
+/**
+ * Interface to algorithms, that finds some number of most distant
+ * vectors.
+ *
+ * @author Yuri Gorshenin
+ * @version 2011.0423
+ * @since 1.6
+ */
 public interface MostDistantInterface {
-    /* Finds m most distant vectors. Returns an array of indexes of
-     * corresponding vectors or null in the case of failure. */
+    /**
+     * Finds indexes of some number of most distant vectors.
+     *
+     * @param vectors array of vectors. Must not be null, all elements must have equal number of dimensions and not be null.
+     * @param m number of most distant vectors, that must be finded. Must be between zero and number of given vectors (inclusive).
+     * @return array of indexes of most distant vectors.
+     */
     int[] findMostDistant(Vector[] vectors, int m);
 }
