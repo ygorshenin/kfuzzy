3b865de5f6a250d5dec4191790808821aebbfd81
ygorshenin <ygorshenin@google.com>
2011-05-05 03:38:35 +0400

    Model now stores output.

--- java/src/kfuzzy/Model.java
+++ java/src/kfuzzy/Model.java
@@ -3,6 +3,7 @@ package kfuzzy;
 import java.util.*;
 
 import kfuzzy.algo.KFuzzyAlgorithm;
+import kfuzzy.io.KFuzzyOutput;
 import kfuzzy.math.Vector;
 
 
@@ -10,12 +11,14 @@ public class Model {
     private KFuzzyAlgorithm algorithm;
     private Vector[] points;
     private int[] clusters;
+    private KFuzzyOutput output;
 
 
     public Model() {
 	algorithm = new KFuzzyAlgorithm();
 	points = new Vector[] {};
 	clusters = new int[] {};
+	output = null;
     }
 
     public void setPoints(Vector[] points) {
@@ -26,6 +29,7 @@ public class Model {
 
     public void clusterize(int numClusters, KFuzzyAlgorithm.Options options) {
 	clusters = algorithm.clusterize(points, numClusters, options);
+	output = new KFuzzyOutput(numClusters, points.length, clusters, options.blending, options.maxIterations);
     }
 
     public Vector[] getPoints() {
@@ -34,5 +38,9 @@ public class Model {
 
     public int[] getClusters() {
 	return clusters;
+    }
+
+    public KFuzzyOutput getOutput() {
+	return output;
     }
 }
